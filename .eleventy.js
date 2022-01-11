8968f0b3eb91beb416ded1a2dc64b870e6cfbaa1
Sophia Dengo <sdengo@gmail.com>
2022-01-10 22:08:35 -0500

    add collection and partial for resume

--- .eleventy.js
+++ .eleventy.js
@@ -7,12 +7,12 @@ const htmlMinTransform = require('./src/transforms/html-min-transform.js');
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = config => {
-  
+
   // Only minify HTML if we are in production because it slows builds _right_ down
   if (isProduction) {
     config.addTransform('htmlmin', htmlMinTransform);
   }
-  
+
   // Returns work items, sorted by display order
   config.addCollection('work', collection => {
     return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
@@ -24,6 +24,12 @@ module.exports = config => {
       x => x.data.featured
     );
   });
+  // Returns a list of resume items ordered by filename
+  config.addCollection('resume', collection => {
+    return collection.getFilteredByGlob('./src/resume/*.md').sort((a, b) => {
+      return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
+    });
+  });
   // Returns a list of people ordered by filename
   config.addCollection('people', collection => {
     return collection.getFilteredByGlob('./src/people/*.md').sort((a, b) => {
@@ -33,7 +39,7 @@ module.exports = config => {
 
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
-  
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
